92eb21e24370da33de961502478941809fd4cb82
YUKIKO IKEDA <applelele@gmail.com>
2020-03-14 16:52:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 def monopoly
 	monopoly = {
 	  :railroads => {
-	    :pieces => "4",
+	    :pieces => 4,
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
 	      :two_pieces_owned => 50,
